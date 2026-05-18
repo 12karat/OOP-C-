2da4a02948e9e2f51c2c5da2914c754213484022
Vitaliy Spirin <vitaliyspirin0@gmail.com>
2026-05-18 20:01:11 +0800

    Add files via upload

---
+++ график.py
@@ -0,0 +1,27 @@
+import time
+import matplotlib.pyplot as plt 
+
+def josephus(n, k):
+    res = 0
+    for i in range(1, n + 1):
+        res = (res + k) % i
+    return res + 1
+
+test_n = [1000, 5000, 10000, 50000, 100000, 500000, 1000000]
+k = 3
+times = []
+
+for n in test_n:
+    start = time.time()
+    result = josephus(n, k)
+    end = time.time()
+    duration = end - start
+    times.append(duration)
+    print(f"N: {n} | Answer: {result} | Time: {duration:.6f} sec")
+
+plt.plot(test_n, times, marker='o')
+plt.xlabel("N (количество людей)")
+plt.ylabel("Время выполнения (сек)")
+plt.title("Задача Иосифа (k = 3)")
+plt.grid()
+plt.show()
